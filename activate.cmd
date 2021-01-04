9aa95e35f08e86fb3aad9ed8985bb9f6d7a83725
Super_QB <q.brala@gmail.com>
2021-01-04 13:30:47 +0100

    Create activate.cmd

---
+++ activate.cmd
@@ -0,0 +1,10 @@
+@echo off
+color a
+slmgr /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
+echo druk op enter om verder te gaan
+pause
+slmgr /skms kms8.msguides.com
+echo druk op enter om verder te gaan
+pause
+slmgr /ato
+echo klaar, druk op enter om script af te sluiten.
